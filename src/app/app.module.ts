b9b0251fbbd24443cd57331302d569382c057bb8
Panchal, Pushpakkumar <pushpak.net@gmail.com>
2020-03-20 00:05:30 -0500

    updating source for news - take 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
 
 import { AgmCoreModule } from '@agm/core';
-import { HttpClientModule, HttpInterceptor } from '@angular/common/http';
+import { HttpClientModule, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { ListOfCountryComponent } from './home/list-of-country/list-of-country.component'
 import { GlobalMapComponent } from './home/global-map/global-map.component';
@@ -23,6 +23,7 @@ import { DatasourceComponent } from './datasource/datasource.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { DeviceDetectorModule } from 'ngx-device-detector';
+import { i1Interceptor, i2Interceptor } from './shared/interceptors/i1';
 
 
 
---
+++ src/app/shared/interceptors/i1.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class i1Interceptor implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>, next: HttpHandler)  {
+    
+        const modified = req.clone({setHeaders : {'custom-header-1' : 'Pushpak'}});
+
+        return next.handle(modified);
+    }
+}
+
+@Injectable()
+export class i2Interceptor implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>, next: HttpHandler)  {
+    
+        const modified = req.clone({setHeaders : {'custom-header-2' : 'Panchal'}});
+
+        return next.handle(modified);
+    }
+}
