61777fe7ddd45e6743bba7c93671c33ab2696f99
Panchal, Pushpakkumar <pushpak.net@gmail.com>
2020-03-20 23:18:39 -0500

    bug on news api

--- src/app/Services/CountryNumbersService.ts
+++ src/app/Services/CountryNumbersService.ts
@@ -284,8 +284,7 @@ export class CountryNumbersService {
    // const url = "https://recipebookapiservice20190223034351.azurewebsites.net/news";
 
        return this.http.get(url, {headers : {'Access-Control-Allow-Origin' :  '*',
-                                             'Access-Control-Allow-Credentials' :  'true', 
-                                             'Access-Control-Allow-Methods' : 'GET,HEAD,OPTIONS,POST,PUT',
+                                             'Access-Control-Allow-Credentials' :  'true',                                             
 'Access-Control-Allow-Headers' : 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'}});
     }
 
